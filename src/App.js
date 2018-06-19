2cb348d4a2f919a8e1639b2b337675f113d3fd36
Ida <ida.bergstrom@medieinstitutet.se>
2018-06-19 18:03:23 +0200

    Theme changer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ class App extends Component {
     
     state = {
         theme: 'defaultTheme',
-        
+        toggleSettings: true
     }
 
+    handleToggle = () => {
+        this.setState({ toggleSettings: !this.state.toggleSettings });
+    }
     
   render() {
+     
+    let settingsClass = 'settingsContainer';
+    if(!this.state.toggleSettings){ settingsClass += ' invisible'; }
     
     return (
 
     <React.Fragment>
-        <div className={'outerContainer ' + this.state.theme }>
+            <div className="fillButtonDiv" onClick={this.handleToggle}>
+                <ion-icon name="color-fill"></ion-icon>
+            </div>
+        
+        
+        
+        <div className={'totalContainer ' + this.state.theme }>
+        
+        <div className={settingsClass}></div>
+        
             <div className="row">
               <div className="widget col-12 col-sm-6 col-md-4 col-ld-4">col</div>
               <div className="widget col-12 col-sm-6 col-md-4 col-ld-4">col</div>
