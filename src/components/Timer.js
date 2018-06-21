f28c608ff9a97bb54fe80dac1e5b6ba79904a3b5
Ida <ida.bergstrom@medieinstitutet.se>
2018-06-21 05:15:39 +0200

    Working on timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,30 +3,40 @@ import React, { Component } from 'react';
 class Timer extends Component {
     
     state = {
-        time: new Date()
+        milliseconds: 300000
       }
 
-      componentDidMount() {
+//      componentDidMount() {
+//        setInterval(
+//          () => this.setTime(),
+//          1000
+//        );
+//      }
+    
+      countdown = () => {
         setInterval(
-          () => this.setTime(),
+          () => this.magic(),
           1000
         );
       }
-    
-      setTime = () => {
-        this.setState({ time: new Date() });
+      
+      magic = () => {
+          this.setState({ milliseconds: this.state.milliseconds - 1000 });
       }
 
   render() {
     
+    
     return (
         <div className="widget col-12 col-sm-6 col-md-4 col-ld-4">
         
-            <div className="innerWidget clockContainer">
+            <div className="innerWidget timerContainer">
                 <span className="iconSize">
-                    <ion-icon name="time"></ion-icon>
+                    <ion-icon name="timer"></ion-icon>
                 </span>
-                { this.state.time.toLocaleTimeString()}
+                { this.state.milliseconds }
+        
+                <button onClick={this.countdown} className="btn btn-primary">Start</button>
             </div>
         
         </div>
